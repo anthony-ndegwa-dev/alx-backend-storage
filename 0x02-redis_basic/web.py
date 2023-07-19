2dc80ff296be8461ef35f4e98ed00cc31093cc1d
root <anthony.n.ndegwa@gmail.com>
2023-07-19 06:43:16 -0700

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,31 +2,30 @@
 """Moldule for request tracking and caching
 """
 import redis
-from functools import wraps
-from typing import Callable
 import requests
-
+from functools import wraps
 
 redis_store = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
+def data_cacher(method):
     """Caches the fetched data."""
     @wraps(method)
-    def invoker(url) -> str:
+    def invoker(url):
         """Wrapper function to cache the output."""
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
+
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    """Returns content of a URL after caching the request's response"""
-    return requests.get(url).text
+    """Returns the content of a URL after caching the request's response."""
+    response = requests.get(url)
+    return response.text
