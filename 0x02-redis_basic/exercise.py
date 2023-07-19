fec2842eba94c089a106dd02fe0d7f33019a4ce6
root <anthony.n.ndegwa@gmail.com>
2023-07-19 05:20:26 -0700

    Edit doc string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""Create a Cache class. In __init__ method, store an instance of the Redis
-client as a private variable named _redis (using redis.Redis()) and flush the
-instance using flushdb.
-
-Create a store method that takes a data argument and returns a string. The
-method should generate a random key (e.g. using uuid), store the input data in
-Redis using the random key and return the key.
-
-Type-annotate store correctly. Remember data can be a str, bytes, int or float
+"""Module to utilize the Redis database, a NoSQL data storage system
 """
 import uuid
 import redis
